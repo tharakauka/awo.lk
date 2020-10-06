1fa3adb7dcdf7e83aee5412407a9a078a7431b44
Tharaka Arachchige <tharakauka@gmail.com>
2020-10-06 17:45:31 +0530

    footer bg color to white

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="topFooter">
         <div class="container text-center">
-            <h4 class="w700 text-white">
+            <h4 class="w700">
                 <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-headset" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M8 1a5 5 0 0 0-5 5v4.5H2V6a6 6 0 1 1 12 0v4.5h-1V6a5 5 0 0 0-5-5z"/>
                 <path d="M11 8a1 1 0 0 1 1-1h2v4a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1V8zM5 8a1 1 0 0 0-1-1H2v4a1 1 0 0 0 1 1h1a1 1 0 0 0 1-1V8z"/>
@@ -13,9 +13,9 @@
         <div class="container mt-5">
             <div class="row align-items-center">
                 <div class="col-12 col-md-3">
-                    <img src="<?php echo HTTP_PATH ?>img/logopng.png" class="img-fluid" alt="">
+                    <img src="<?php echo HTTP_PATH ?>img/logo.png" class="img-fluid" alt="">
                 </div>
-                <div class="col-12 col-md-9 border-left border-light">
+                <div class="col-12 col-md-9 border-left border-dark">
                     <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
                 </div>
             </div>
